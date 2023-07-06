adbcd67de39da2e3588969c7957ec187b6e7a9eb
joaoalcimar <joao.alcimar.junior@gmail.com>
2023-07-06 13:09:06 -0300

    Implement server status endpoint

--- sales-api/app.js
+++ sales-api/app.js
@@ -4,8 +4,16 @@ const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
+app.get('/api/status', (req, res) => {
+    return res.status(200).json({
+        service: 'Auth API',
+        status: "up",
+        httpStatus: 200
+    })
+})
+
 app.listen(PORT, () => {
-    console.info();
+    console.info(`Server started succcessfuly at port ${PORT}`);
 })
 
 
