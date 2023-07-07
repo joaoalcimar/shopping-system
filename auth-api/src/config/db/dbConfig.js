db16476d71521a959219bec2d4ba342dee681d65
joaoalcimar <joao.alcimar.junior@gmail.com>
2023-07-07 12:45:05 -0300

    Implement initial data insertion on database

--- auth-api/src/config/db/dbConfig.js
+++ auth-api/src/config/db/dbConfig.js
@@ -1,7 +1,8 @@
 import {Sequelize} from "sequelize";
 
-const sequelize = new Sequelize("auth-db","postgres","123456", {
+const sequelize = new Sequelize("auth-db","admin","123456", {
     host: "localhost",
+    port: "5434",
     dialect: "postgres",
     quoteIdentifiers: false,
     define:{
---
+++ auth-api/src/modules/user/model/User.js
@@ -0,0 +1,23 @@
+import {Sequelize, DataTypes} from "sequelize";
+import sequelize from "../../../config/db/dbConfig.js";
+
+const User = sequelize.define("user", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+export default User;
