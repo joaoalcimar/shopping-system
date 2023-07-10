4e23b0e7d48d41622b94d721cea56a5b246bf0b5
joaoalcimar <joao.alcimar.junior@gmail.com>
2023-07-10 15:56:56 -0300

    Implement repositories

---
+++ product-api/src/main/java/br/com/productapi/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package br.com.productapi.repositories;
+
+import br.com.productapi.models.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
