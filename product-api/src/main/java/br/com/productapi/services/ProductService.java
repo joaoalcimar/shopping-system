f79e557a39b8ea4c0380422aa2810b3fea6f2872
joaoalcimar <joao.alcimar.junior@gmail.com>
2023-07-10 15:59:01 -0300

    Create product base entities

---
+++ product-api/src/main/java/br/com/productapi/services/ProductService.java
@@ -0,0 +1,12 @@
+package br.com.productapi.services;
+
+import br.com.productapi.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+}
