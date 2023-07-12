90fcb5c99c6b02f58d06b59599d3f8bcbbdb11a8
joaoalcimar <joao.alcimar.junior@gmail.com>
2023-07-12 09:59:35 -0300

    Remove unused import

--- product-api/src/main/java/br/com/productapi/repositories/ProductRepository.java
+++ product-api/src/main/java/br/com/productapi/repositories/ProductRepository.java
@@ -1,6 +1,5 @@
 package br.com.productapi.repositories;
 
-import br.com.productapi.models.dtos.responses.ProductResponse;
 import br.com.productapi.models.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
