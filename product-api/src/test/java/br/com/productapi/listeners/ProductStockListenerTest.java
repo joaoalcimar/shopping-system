8bea719241460dc51c27da5668c8e6da7f60e2df
joaoalcimar <joao.alcimar.junior@gmail.com>
2023-07-23 19:04:40 -0300

    Test listeners

--- product-api/src/test/java/br/com/productapi/listeners/ProductStockListenerTest.java
+++ product-api/src/test/java/br/com/productapi/listeners/ProductStockListenerTest.java
@@ -1,5 +1,53 @@
 package br.com.productapi.listeners;
 
-public class ProductStockListenerTest
-{
+import br.com.productapi.models.dtos.ProductStockDTO;
+import br.com.productapi.services.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ProductStockListenerTest {
+
+    @Mock
+    private ProductService productService;
+
+    @InjectMocks
+    private ProductStockListener productStockListener;
+
+    @Captor
+    private ArgumentCaptor<ProductStockDTO> productStockDTOCaptor;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testReceiveProductStockMessage() throws JsonProcessingException {
+        // Create a sample ProductStockDTO
+        ProductStockDTO productStockDTO = new ProductStockDTO();
+        productStockDTO.setSalesId("{\"salesId\":\"test123\"}");
+        productStockDTO.setProducts(new ArrayList<>());
+
+        // Convert the ProductStockDTO to JSON string
+        String productStockJson = new ObjectMapper().writeValueAsString(productStockDTO);
+
+        // Call the receiveProductStockMessage method with the JSON message
+        productStockListener.receiveProductStockMessage(productStockDTO);
+
+        // Verify that the productService.updateProductStock method is called with the correct argument
+        verify(productService, times(1)).updateProductStock(productStockDTOCaptor.capture());
+
+        // Check the captured argument and verify its content
+        ProductStockDTO capturedProductStockDTO = productStockDTOCaptor.getValue();
+        assertEquals(productStockDTO.getProducts(), capturedProductStockDTO.getProducts());
+        assertEquals(productStockDTO.getSalesId(), capturedProductStockDTO.getSalesId());
+    }
 }
